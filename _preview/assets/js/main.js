472c722da955967035b663eea2055d1391257687
lorenz <50631871+lorenzonavarini@users.noreply.github.com>
2026-07-17 15:25:44 +0200

    Publish link and profile updates

--- _preview/assets/js/main.js
+++ _preview/assets/js/main.js
@@ -1,1 +1,12 @@
-document.querySelectorAll('a[href]').forEach(function(link){if(!link.getAttribute('href').startsWith('#')){link.target='_blank';link.rel='noopener noreferrer'}});
+document.querySelectorAll('a[href]').forEach(function (link) {
+  var url = new URL(link.getAttribute('href'), window.location.href);
+  var isExternalWebsite = (url.protocol === 'http:' || url.protocol === 'https:') && url.origin !== window.location.origin;
+
+  if (isExternalWebsite) {
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+  } else {
+    link.removeAttribute('target');
+    link.removeAttribute('rel');
+  }
+});
